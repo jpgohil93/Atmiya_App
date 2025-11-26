a964ee06ff0a99b84a2d491980de5e9151b8bf1b
jpgohel93 <jpgohel93@gmail.com>
2025-11-27 02:00:06 +0530

    Update compileSdk and targetSdk to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.atmiya.innovation"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.atmiya.innovation"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
