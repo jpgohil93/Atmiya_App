7d47df0fcd73e5648978335863cd1b6bf4742433
jpgohel93 <jpgohel93@gmail.com>
2025-11-27 12:26:22 +0530

    Fix Invalid Package: Correct aggressive packaging excludes to preserve signature files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,8 +81,10 @@ android {
 
     packaging {
         resources {
-            excludes += "/META-INF/**"
-            excludes += "META-INF/*"
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "META-INF/LICENSE*"
+            excludes += "META-INF/NOTICE*"
+            excludes += "META-INF/*.kotlin_module"
         }
 
     }
